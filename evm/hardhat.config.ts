349c4eb3cae48eccae0237f1a5e359aa30b78c05
isthispalash <thisispalash@kdio.xyz>
2024-10-20 08:40:30 -0700

    add networks

--- evm/hardhat.config.ts
+++ evm/hardhat.config.ts
@@ -1,8 +1,18 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.27",
+  solidity: "0.8.23",
+  networks: {
+    story: { // illiad
+      url: "https://testnet.storyrpc.io/",
+      accounts: [vars.get("PRIVATE_KEY")],
+    },
+    flow: { // evm on flow
+      url: "https://testnet.evm.nodes.onflow.org",
+      accounts: [vars.get("PRIVATE_KEY")],
+    },
+  },
 };
 
 export default config;
